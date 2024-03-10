33255d4a0d3d7f2ba9347a4c9bee940e83e15df4
Yehor Radchenko <y.p.radchenko@gmail.com>
2024-03-10 13:28:30 +0200

    Add project files.

---
+++ TestTaskCrypto/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TestTaskCrypto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
